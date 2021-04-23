18977966889d6e5876c448cecb457370f1206b3b
TH <1260597728@qq.com>
2021-04-23 18:35:57 +0800

    article model commit

--- models/article.go
+++ models/article.go
@@ -14,8 +14,42 @@ type Article struct {
 	State      int    `json:"state"`
 }
 
-//func GetArticles(pageNum, pageSize int, maps interface{}) []Article {
-//	var articles []Article
-//	DBEngine
-//}
+
+// 根据条件获取所有文章
+func GetArticles(pageNum, pageSize int, maps interface{}) []Article {
+	var articles []Article
+	DBEngine.Where(maps).Limit(pageSize, pageNum).Find(&articles)
+
+	return articles
+}
+
+// 根据条件获取文章总数
+func GetArticleToTal(maps interface{}) (count int) {
+	DBEngine.Table(new(Article)).Where(maps).Count(&count)
+
+	return
+}
+
+// 通过id获取单篇文章
+func GetArticleById(id int) (article *Article) {
+	DBEngine.ID(id).Get(article)
+
+	return
+}
+
+// 添加文章
+func AddArticle(data map[string]interface{}) bool {
+	article := &Article{
+		TagId: data["tag_id"].(int),
+	}
+	if articleId, err := DBEngine.Insert(article); articleId > 0 && err == nil {
+		return true
+	}
+
+	return false
+}
+
+func EditArticle(id int, maps interface{}) bool {
+	return false
+}
 
